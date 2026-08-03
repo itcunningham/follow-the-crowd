67c269e0221a9a7109fd9456e1567212753a7021
itcunningham <itcunningham99@gmail.com>
2026-08-03 20:20:26 +1000

    Revert "TEMPORARY: trace the committed unread Set, not just the render"
    This reverts commit 4cd5de87fae0ddfcb51671d253836a9bfdc74981.

--- app/dm/page.tsx
+++ app/dm/page.tsx
@@ -677,15 +677,6 @@ function DmInboxPageContent() {
   }, [refreshUnreadState]);
 
   useEffect(() => {
-    unreadTrace("2-unread-set-committed", {
-      eventSetContents: [...unreadEventChatIds],
-      eventSetContentsJson: [...unreadEventChatIds].map((id) => JSON.stringify(id)),
-      groupUnreadCount: unreadEventChatIds.size,
-      conversationSetSize: unreadConversationIds.size,
-    });
-  }, [unreadEventChatIds, unreadConversationIds]);
-
-  useEffect(() => {
     function handleReadsUpdated() {
       void refreshUnreadState();
     }
